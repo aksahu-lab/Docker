5ad3af9fafde38c32e6e81ef484a72294de14344
gyana.gouda <gp.ios91@gmail.com>
2023-09-24 22:52:37 +0530

    removed unwanted packages

--- src/models/album.js
+++ src/models/album.js
@@ -1,40 +1,58 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const albumSchema = new mongoose.Schema({
+const albumSchema = new mongoose.Schema(
+  {
     albumName: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     status: {
-        type: String,
-        enum: ['draft', 'inprogress', 'in-review', 'completed'],
-        default: 'draft'
+      type: String,
+      enum: ["draft", "inprogress", "in-review", "completed"],
+      default: "draft",
     },
     eventType: {
-        type: String,
-        required: true,
-        enum: [ "Half Saree", "Engagment", "Pre-Wedding", "Wedding", "Post-Wedding", "Maternity and Newborn", "Birthday", "Corporate Function", "Portraits", "Fashion", "Food Photography", "Lifestyle", "Sports", "Other" ]
+      type: String,
+      required: true,
+      enum: [
+        "Half Saree",
+        "Engagement",
+        "Pre-Wedding",
+        "Wedding",
+        "Post-Wedding",
+        "Maternity and Newborn",
+        "Birthday",
+        "Corporate Function",
+        "Portraits",
+        "Fashion",
+        "Food Photography",
+        "Lifestyle",
+        "Sports",
+        "Other",
+      ],
     },
     eventDate: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     studio: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Studio'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Studio",
     },
     client: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    }
-}, {
-    timestamps: true
-})
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Album = mongoose.model('Album', albumSchema)
+const Album = mongoose.model("Album", albumSchema);
 
-module.exports = Album
+module.exports = Album;
